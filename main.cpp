88606bf3e93f617cab6d2ea0cfe59aa0da2b8fbe
AgarwalMayank2 <mayankagarwal020905@gmail.com>
2024-10-04 01:00:11 +0530

    consolidate function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 //--------First we will do Fibonacci Heap implementation--------
@@ -26,17 +27,62 @@ public:
 
 class FibonacciHeap() {
 private:
-    
+
+    void consolidate() { //Function that we are using for connecting the nodes which have same degrees
+        int d = (int)log2(NodeCount)+1; //Maximum number of degree that can be
+        vector<Node*> degree_table(d, nullptr); //Vector to store the nodes of specific degrees
+
+        vector<Node*> nodes;
+        Node* x = min_node;
+
+        do{ //Pushing all the nodes to the nodes vector
+            nodes.push_back(x);
+            x = x->right;
+        } while(x != min_node)
+
+        for (Node* w : nodes) {
+            Node* x = w;
+            d = x->degree;
+            while(degree_table[d]) { //Merging all the nodes with same degrees in the degree_table vector
+                Node* y = degree_table[d];
+                if (x->key > y->key) {
+                    Node* temp = x;
+                    x = y;
+                    y = temp;
+                }
+                //link(y, x);
+                degree_table[d] = nullptr;
+                d++;
+            }
+            degree_table[d] = x;
+        }
+
+        //Making a new heap with the merged nodes that is the final step for consolidating the heap
+        min_node = nullptr;
+        for (auto& w : degree_table) {
+            if (w) {
+                if (!min_node) min_node = w;
+                else {
+                    (min_node->left)->right = w;
+                    w->right = min_node;
+                    w->left = min_node->left;
+                    min_node->left = w;
+                    if (min_node->key > w->key) min_node = w;
+                }
+            }
+        }
+    }
+
 public:
-    Node* min_node;
-    int NodeCount;
+    Node* min_node; //We can also say it as a root of the heap
+    int NodeCount; //Number of nodes in the whole heap
 
     FibonacciHeap() {
         min_node = nullptr;
         NodeCount = 0;
     }
 
-    void insert(Node* node) {
+    void insert(Node* node) { // We will use this function to insert a node in the heap
         if (!min_node) {
             min_node = node;
         }
@@ -51,17 +97,19 @@ public:
         return;
     }
 
-    Node* findMin() {
+    Node* findMin() { //Function for just getting the minimum node
         return min_node;
     }
 
-    Node* extract_min() {
+    Node* extract_min() { //We will use this function to get and remove the minimum node in the heap
         if (!min_node) return nullptr;
         Node* temp = min_node;
         Node* ptr = temp;
         Node* x = NULL;
 
         if (temp->child) {
+            //Here we will be taking each child of the min_node and insert it to the left of the min_node and after that
+            //we will just remove the min_node and make a new min_node
 
             x = temp->child;
             do {
@@ -82,7 +130,7 @@ public:
         }
         else {
             min_node = temp->right;
-            //consolidate();
+            consolidate();
         }
         NodeCount--;
         return temp;
